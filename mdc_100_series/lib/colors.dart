15f2ba8cb88febc895bfc534a66b27eaf05df78d
chamidahazi <chamidahazimatul@gmail.com>
2024-06-29 15:28:34 +0700

    praktikum6

---
+++ mdc_100_series/lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kShrinePink50 = Color.fromARGB(255, 25, 102, 184);
+const kShrinePink100 = Color.fromARGB(255, 32, 192, 195);
+const kShrinePink300 = Color.fromARGB(255, 24, 113, 135);
+const kShrinePink400 = Color.fromARGB(255, 17, 165, 165);
+
+const kShrineBrown900 = Color(0xFF442B2D);
+
+const kShrineErrorRed = Color(0xFFC5032B);
+
+const kShrineSurfaceWhite = Color(0xFFFFFBFA);
+const kShrineBackgroundWhite = Colors.white;
+const kShrinePurple = Color.fromARGB(255, 11, 167, 63);
